7d9905464130184015796f244998e6ae0ff6e828
Sumit Chaturvedi <sumit.chaturvedi@gmail.com>
2024-08-11 23:25:19 +0000

    A

---
+++ run_test.sh
@@ -0,0 +1,3 @@
+python3 track_hand_signer.py --video_path tests/test_1.mp4 --save_to outputs/test_1.mp4
+python3 track_hand_signer.py --video_path tests/test_2.mp4 --save_to outputs/test_2.mp4
+python3 track_hand_signer.py --video_path tests/test_3.mp4 --save_to outputs/test_3.mp4
